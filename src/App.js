4b3ba880e77fcbe176930cac6375ce8d48e10d32
Milind-dev <milinddev1011@gmail.com>
2026-01-07 17:58:35 +0530

    component add

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import Album4 from './Album4';
 // import AlbumSearchdrop from './AlbumSearchdrop';
 import AlbumSearch from "./AlbumSearch";
 import Album1Debounce from './FinalCode/Album1Debounce';
+import FakeStore from './interview/FakeStore';
 
 function App() {
   return (
     <div className="App">
+      {/* albums debounce, sort, search */}
       {/* <Album3 /> */}
       {/* <Album2 /> */}
       {/* <Album4 /> */}
       {/* <AlbumSearchdrop /> */}
       {/* <AlbumSearch /> */}
-      <Album1Debounce />
+      {/* <Album1Debounce /> */}
+  {/* ================================ */}
+    {/* https://fakestoreapi.com/products */}
+      <FakeStore />
+
+
     </div>
   );
 }
